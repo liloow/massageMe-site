d839a0e3b182cf17034a6ede8a29c2f8c6c1cd49
liloow <tom.beguin@gmail.com>
2018-08-18 18:46:56 +0200

    test build

--- controllers/db.js
+++ controllers/db.js
@@ -2,7 +2,7 @@ const colors = require('colors');
 const pgp = require('pg-promise');
 const promise = require('bluebird');
 const diagnostics = require('./dbDiag');
-const fs = require('fs');
+// const fs = require('fs');
 {
   colors; // To shut up eslint
 }
@@ -17,12 +17,12 @@ const config = {
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  ssl: {
-    rejectUnauthorized: false,
-    ca: fs.readFileSync('/home/tom/.postgresql/root.crt').toString(),
-    key: fs.readFileSync('/home/tom/.postgresql/postgresql.key').toString(),
-    cert: fs.readFileSync('/home/tom/.postgresql/postgresql.crt').toString(),
-  },
+  // ssl: {
+  //   rejectUnauthorized: false,
+  //   ca: fs.readFileSync('/home/tom/.postgresql/root.crt').toString(),
+  //   key: fs.readFileSync('/home/tom/.postgresql/postgresql.key').toString(),
+  //   cert: fs.readFileSync('/home/tom/.postgresql/postgresql.crt').toString(),
+  // },
 };
 
 // Create the database instance:
