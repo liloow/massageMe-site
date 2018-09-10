e9d6c5fc958d8ecf8dfef739ad01f8a69162f3c4
liloow <tom.beguin@gmail.com>
2018-09-11 01:17:55 +0200

    api

--- src/api.js
+++ src/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL:
-    process.env.NODE_ENV === 'production' ? 'http://localhost:3000/' : 'http://localhost:3000/',
+  baseURL: process.env.NODE_ENV === 'production' ? '/' : 'http://localhost:3000/',
   withCredentials: true,
   responseType: 'json',
 });
